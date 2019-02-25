a86c9b148d22e335bc2dda8ee38cf6f4c6c98c5f
orlandoacosta99 <36582050+orlandoacosta99@users.noreply.github.com>
2019-02-25 17:19:56 -0500

    Update pruebas.py
    se realizo unificacion de rama jgalvis

--- pruebas/pruebas.py
+++ pruebas/pruebas.py
@@ -4,47 +4,57 @@ import funciones as f
 class pruebas(unittest.TestCase):
 
     def test_calcular_precio_producto(self):
-        self.assertEqual(f.calcular_precio_producto(1000), 1500.0)
-        self.assertEqual(f.calcular_precio_producto(2000), 3000.0)
-        self.assertEqual(f.calcular_precio_producto(0), 0)
+        self.assertEqual(f.calcular_precio_producto(1000), 1500)
+        self.assertEqual(f.calcular_precio_producto(1500), 2250)
+        self.assertEqual(f.calcular_precio_producto(0), 'El costo del producto debe ser mayor a cero.')
+        self.assertEqual(f.calcular_precio_producto(-200), 'El costo del producto debe ser mayor a cero.')
         pass
 
     def test_calcular_precio_servicio(self):
-        self.assertEqual(f.calcular_precio_servicio(4), 400000.0)
-        self.assertEqual(f.calcular_precio_servicio(5), 500000.0)
-        self.assertEqual(f.calcular_precio_servicio(0), 0)
+        self.assertEqual(f.calcular_precio_servicio(5), 500000)
+        self.assertEqual(f.calcular_precio_servicio(3), 300000)
+        self.assertEqual(f.calcular_precio_servicio(0), 'Las horas deben ser mayor a cero.')
+        self.assertEqual(f.calcular_precio_servicio(-2), 'Las horas deben ser mayor a cero.')
         pass
 
     def test_calcular_precio_servicio_extras(self):
-        self.assertEqual(f.calcular_precio_servicio_extras(1, 4), 525000.0)
-        self.assertEqual(f.calcular_precio_servicio_extras(2, 1), 350000.0)
-
+        self.assertEqual(f.calcular_precio_servicio_extras(2), 250000)
+        self.assertEqual(f.calcular_precio_servicio_extras(0), 'El servicio no genera horas extras.')
+        self.assertEqual(f.calcular_precio_servicio_extras(-1), 'Las horas deben ser mayor a cero.')
         pass
 
     def test_calcular_costo_envio(self):
-        self.assertEqual(f.calcular_costo_envio(1), 115.0)
-        self.assertEqual(f.calcular_costo_envio(0), 0.0)
-
+        self.assertEqual(f.calcular_costo_envio(2), 230)
+        self.assertEqual(f.calcular_costo_envio(0), 0)
+        self.assertEqual(f.calcular_costo_envio(-2), 'El numero de kilometros debe ser mayor a cero.')
         pass
 
     def test_calcular_precio_producto_fuera(self):
-        self.assertEqual(f.calcular_precio_producto_fuera(1000, 1), 1615.0)
-        self.assertEqual(f.calcular_precio_producto_fuera(2000, 1), 3115.0)
+        self.assertEqual(f.calcular_precio_producto_fuera(600, 20), 3200)
+        self.assertEqual(f.calcular_precio_producto_fuera(2000, 50), 8750)
+        self.assertEqual(f.calcular_precio_producto_fuera(1100, 0), 1650)
+        self.assertEqual(f.calcular_precio_producto_fuera(1100, 0), 1650)
+        self.assertEqual(f.calcular_precio_producto_fuera(0, 10), 'El costo del producto debe ser mayor a cero.')
         pass
 
     def test_calcular_iva_producto(self):
-        self.assertEqual(f.calcular_iva_producto(1000, 0.19), 190.0)
-        self.assertEqual(f.calcular_iva_producto(2000, 0.19), 380.0)
+        self.assertEqual(f.calcular_iva_producto(1000, 0.19), 285)
+        self.assertEqual(f.calcular_iva_producto(1000, 0), 0)
+        self.assertEqual(f.calcular_iva_producto(0, 0), 'El costo del producto debe ser mayor a cero.')
         pass
 
     def test_calcular_iva_servicio(self):
-        self.assertEqual(f.calcular_iva_servicio(4, 0.19), 76000.0)
-        self.assertEqual(f.calcular_iva_servicio(2, 0.16), 32000.0)
+        self.assertEqual(f.calcular_iva_servicio(4, 0.19), 76000)
+        self.assertEqual(f.calcular_iva_servicio(4, 0), 0)
+        self.assertEqual(f.calcular_iva_servicio(0, 0.19), 'El numero de horas debe ser mayor a cero.')
         pass
 
     def test_calcular_iva_envio(self):
-        self.assertEqual(f.calcular_iva_envio(1, 0.19), 21.85)
-        self.assertEqual(f.calcular_iva_envio(0, 0.19), 0.0)
+        self.assertEqual(f.calcular_iva_envio(2, 0.19), 43.7)
+        self.assertEqual(f.calcular_iva_envio(8, 0.19), 174.8)
+        self.assertEqual(f.calcular_iva_envio(0, 0.19), 0)
+        self.assertEqual(f.calcular_iva_envio(4, 0), 0)
+        self.assertEqual(f.calcular_iva_envio(-3, 0.19), 'El numero de kilometros no debe ser menor a cero.')
         pass
 
     def test_calcular_iva_servicio_fuera(self):
@@ -54,7 +64,6 @@ class pruebas(unittest.TestCase):
 
     def test_calcular_recaudo_locales(self):
         self.assertEqual(f.calcular_recaudo_locales(1000, 1500, 2000), 4500.0)
-        self.assertEqual(f.calcular_recaudo_locales(0, 1500, 0), 1500.0)
         pass
 
     """
